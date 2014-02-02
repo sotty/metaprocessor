2f11372412c31434522815a3919f29840fe99309
Demian Calcaprina <calcacuervo@gmail.com>
2014-02-02 16:41:14 -0300

    First commit.

---
+++ helper/src/main/java/metaprocessor/App.java
@@ -0,0 +1,13 @@
+package metaprocessor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
