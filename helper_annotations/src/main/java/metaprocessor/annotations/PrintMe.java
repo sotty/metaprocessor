a58c93aaf593917991d2f1ced8d5e8290b485242
nicolasegomez <nicolas.e.gomez@gmail.com>
2014-02-06 19:36:58 -0300

    Se divide en 3 proyectos porque es necesario que uno compile usando los otros

---
+++ helper_annotations/src/main/java/metaprocessor/annotations/PrintMe.java
@@ -0,0 +1,7 @@
+package metaprocessor.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.SOURCE)
+public @interface PrintMe {}
