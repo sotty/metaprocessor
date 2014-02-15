ee5236dd968f27a9e17084c28602ee240af5c599
nicolasegomez <nicolas.e.gomez@gmail.com>
2014-02-15 12:55:26 -0300

    Se agrega el generador de codigo

--- helper_annotations_processors/src/main/java/metaprocessor/PrintMeAnnotationProcessor.java
+++ helper_annotations_processors/src/main/java/metaprocessor/PrintMeAnnotationProcessor.java
@@ -1,5 +1,7 @@
 package metaprocessor;
 
+import java.io.IOException;
+import java.io.Writer;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -8,19 +10,44 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
+import javax.tools.JavaFileObject;
+
+import metaprocessor.codegenerator.MetaCodeGenerator;
 
 @SupportedAnnotationTypes({ "metaprocessor.annotations.PrintMe" })
 public class PrintMeAnnotationProcessor extends AbstractProcessor {
 
 	public boolean process(Set<? extends TypeElement> annotations,
 			RoundEnvironment env) {
+
 		Messager messager = processingEnv.getMessager();
+		messager.printMessage(Diagnostic.Kind.ERROR,
+				"Starting annotation processing.");
 		for (TypeElement te : annotations) {
 			for (Element e : env.getElementsAnnotatedWith(te)) {
 				messager.printMessage(Diagnostic.Kind.NOTE,
 						"Printing: " + e.toString());
+				if (e.getKind() == ElementKind.CLASS) {
+					TypeElement classElement = (TypeElement) e;
+					
+					JavaFileObject jfo;
+					try {
+						jfo = processingEnv.getFiler().createSourceFile(
+								classElement.getQualifiedName() + "_");
+
+						Writer writer = jfo.openWriter();
+						new MetaCodeGenerator().generateCode(writer, classElement);
+						writer.close();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+
+				}
 			}
 		}
 		return true;
---
+++ helper_annotations_processors/src/main/java/metaprocessor/codegenerator/MetaCodeGenerator.java
@@ -0,0 +1,41 @@
+package metaprocessor.codegenerator;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Set;
+
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.PackageElement;
+import javax.lang.model.element.TypeElement;
+
+public class MetaCodeGenerator {
+	
+	public void generateCode(Writer writer, TypeElement classElement) throws IOException{
+		PackageElement packageElement = (PackageElement) classElement
+				.getEnclosingElement();
+		 BufferedWriter bw = new BufferedWriter(writer);
+	     bw.append("package ");
+	     bw.append(packageElement.getQualifiedName());
+	     bw.append(";");
+	     bw.newLine();
+	     bw.newLine();
+	     bw.append("public class " + classElement.getSimpleName() + "_ " + "{");
+	     bw.newLine();
+//	     bw.append("public boolean test() {");
+//	     bw.newLine();
+//	     bw.append("return true;");
+//	     bw.newLine();
+//	     bw.append("}");
+	     bw.newLine();
+	     
+	     bw.append("}");
+	     bw.newLine();
+	     bw.newLine();
+	     bw.flush();
+	     bw.close();
+	     
+	     // rest of generated class contents
+	}
+
+}
