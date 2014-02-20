6454775326b9a9651ed13c636c269556407696e0
Demian Calcaprina <calcacuervo@gmail.com>
2014-02-19 23:22:08 -0300

    Log something in the test.

--- helper/src/test/java/metaprocessor/AppTest.java
+++ helper/src/test/java/metaprocessor/AppTest.java
@@ -7,32 +7,36 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
+	/**
+	 * Create the test case
+	 * 
+	 * @param testName
+	 *            name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	/**
+	 * Rigourous Test :-)
+	 * 
+	 * @throws ClassNotFoundException
+	 * @throws SecurityException
+	 */
+	public void testApp() throws SecurityException, ClassNotFoundException {
+		assertEquals(1, Class.forName("metaprocessor.PrintMeAP_Test_")
+				.getDeclaredMethods().length);
+		System.out
+				.println("The generated class has only one method, with name: "
+						+ Class.forName("metaprocessor.PrintMeAP_Test_")
+								.getDeclaredMethods()[0].getName());
+	}
 }
